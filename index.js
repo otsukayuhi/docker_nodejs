b7280137de02f5815f4860f7eef8357470886c3f
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-10-19 12:16:01 +0900

    お魚ちゃんを追加🐟

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! 🐟"));
 
 app.listen(3000, () => console.log("Example app: http://localhost:3000"));
